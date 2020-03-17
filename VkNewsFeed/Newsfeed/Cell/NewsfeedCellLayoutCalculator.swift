f62617711cbdc37104417fd61a037cf52fbe16fe
Timur Korolev <timurkorolev89@gmail.com>
2020-03-17 19:07:54 +0300

    Auto Sizing Cells Done

--- VkNewsFeed/Newsfeed/Cell/NewsfeedCellLayoutCalculator.swift
+++ VkNewsFeed/Newsfeed/Cell/NewsfeedCellLayoutCalculator.swift
@@ -16,7 +16,7 @@ protocol FeedCellLayoutCalculatorProtocol {
 struct Sizes: FeedCellSizes {
     var postLabelFrame: CGRect
     var attachmentFrame: CGRect
-    var bottomView: CGRect
+    var bottomViewFrame: CGRect
     var totalHeight: CGFloat
 }
 
@@ -25,6 +25,7 @@ struct Constants {
     static let topViewHeight: CGFloat = 36
     static let postLabelInsets = UIEdgeInsets(top: 8 + Constants.topViewHeight + 8, left: 8, bottom: 8, right: 8)
     static let postLabelFont = UIFont.systemFont(ofSize: 15)
+    static let bottomViewHeight: CGFloat = 58
 }
 
 final class NewsfeedCellLayoutCalculator: FeedCellLayoutCalculatorProtocol {
@@ -54,10 +55,31 @@ final class NewsfeedCellLayoutCalculator: FeedCellLayoutCalculatorProtocol {
         
         //MARK: - Work with attachmentFrame
         
+        let attachmentTop = postLabelFrame.size == CGSize.zero ? Constants.postLabelInsets.top : postLabelFrame.maxY + Constants.postLabelInsets.bottom
+        
+        var attachmentFrame = CGRect(origin: CGPoint(x: 0, y: attachmentTop),
+                                     size: CGSize.zero)
+        
+        if let attachment = phootoAttachment {
+            let ratio = CGFloat(attachment.height) / CGFloat(attachment.width)
+            attachmentFrame.size = CGSize(width: cardViewWidth, height: cardViewWidth * ratio)
+        }
+        
+        //MARK: - Work with bottomViewFrame
+        
+        let bottomViewTop = max(postLabelFrame.maxY, attachmentFrame.maxY)
+        
+        let bottomViewFrame = CGRect(origin: CGPoint(x: 0, y: bottomViewTop),
+                                     size: CGSize(width: cardViewWidth, height: Constants.bottomViewHeight))
+        
+        //MARK: - Work with totalHeight
+        
+        let totalHeight = bottomViewFrame.maxY + Constants.cardInsets.bottom
+        
         return Sizes(postLabelFrame: postLabelFrame,
-                     attachmentFrame: CGRect.zero,
-                     bottomView: CGRect.zero,
-                     totalHeight: 300)
+                     attachmentFrame: attachmentFrame,
+                     bottomViewFrame: bottomViewFrame,
+                     totalHeight: totalHeight)
     }
     
 }
