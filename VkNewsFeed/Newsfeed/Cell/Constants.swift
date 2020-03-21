b83ca72a5b4453c5529df719f71452db66cab492
Timur Korolev <timurkorolev89@gmail.com>
2020-03-21 13:54:52 +0300

    minor fix constants

--- VkNewsFeed/Newsfeed/Cell/Constants.swift
+++ VkNewsFeed/Newsfeed/Cell/Constants.swift
@@ -11,10 +11,10 @@ import UIKit
 
 struct Constants {
     static let cardInsets = UIEdgeInsets(top: 0, left: 8, bottom: 12, right: 8)
-    static let topViewHeight: CGFloat = 36
+    static let topViewHeight: CGFloat = 60
     static let postLabelInsets = UIEdgeInsets(top: 8 + Constants.topViewHeight + 8, left: 8, bottom: 8, right: 8)
     static let postLabelFont = UIFont.systemFont(ofSize: 15)
-    static let bottomViewHeight: CGFloat = 58
+    static let bottomViewHeight: CGFloat = 44
     
     static let bottomViewViewHight: CGFloat = 44
     static let bottomViewViewWidth: CGFloat = 80
